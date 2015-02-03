3e549bc89928eb133ac1e268c17d2643d40d82cb
guyfawkes5 <mikewalsh2@gmail.com>
2015-02-03 14:55:08 +0000

    Adding changes to static component.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,7 +1,16 @@
 angular.module('notesApp', []).controller('MainCtrl', [function() {
     var self = this;
-    self.message= 'Hello ';
-    self.changeMessage = function() {
-        self.message = 'Goodbye';
-    };
+    self.notes = [{
+        id: 1,
+        label: 'First Note',
+        done: false
+    }, {
+        id: 2,
+        label: 'Second Note',
+        done: false
+    }, {
+        id: 3,
+        label: 'Finished Third Note',
+        done: true
+    }];
 }]);
